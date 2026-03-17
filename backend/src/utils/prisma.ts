fc11aa85c9060352d9e151def30f6af87676a303
nashriq420 <nshrqirfn@gmail.com>
2026-03-17 15:22:37 +0800

    fix Pool typo

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -4,7 +4,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 
 const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:5432/greenery?schema=public';
 const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg(pool as any);
 
 export const prisma = new PrismaClient({ adapter });
 
