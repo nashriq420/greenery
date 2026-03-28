09aabadae8a501ff873928b4a6ed716999d0f833
nashriq420 <nshrqirfn@gmail.com>
2026-03-28 23:12:13 +0800

    chore: untrack ignored files and sync local state

--- backend/dist/routes/subscription.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const subscription_controller_1 = require("../controllers/subscription.controller");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const router = (0, express_1.Router)();
-router.post('/upgrade', auth_middleware_1.authenticateToken, subscription_controller_1.upgradeSubscription);
-router.get('/status', auth_middleware_1.authenticateToken, subscription_controller_1.getSubscriptionStatus);
-exports.default = router;
